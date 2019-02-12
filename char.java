2675d263bcba3e73a3d119b7c3c7ac963835ebf2
mohammedaafith <aafith007@gmail.com>
2019-02-12 11:53:49 +0530

    Update char.java

--- char.java
+++ char.java
@@ -1,14 +1,16 @@
 public class Count {
-  int i=0;
+	public static void main(String[ ] arg)
+	{
+        int i=0;
 	Scanner sc=new Scanner(System.in);
 	System.out.println("Enter a character : ");
 	char ch=sc.next( ).charAt(0);	 		
 	if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U')
 	{
-	System.out.println("\n Entered character "+ch+" is  Vowel"); 
+	System.out.println("\n "+ch+" is  Vowel"); 
 	}
 	else if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z'))
-		System.out.println("Entered character "+ch+" is Consonant");
+		System.out.println(" "+ch+" is Consonant");
 	      else
 		System.out.println("\n Its is invalid");		
 	}
