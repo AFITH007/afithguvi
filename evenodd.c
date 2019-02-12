03ca85d7b042b44b4d1c56aa971559f60f169af3
mohammedaafith <aafith007@gmail.com>
2019-02-12 11:42:05 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -5,8 +5,8 @@ int num;
 printf("\n ENTER THE NUMBER");
 scanf("%d",&num);
 if(num%2==0)
- printf("\n It is even");
+ printf("\n It is valid");
  else
- printf("\n It is odd");
+ printf("\n It is invalid");
  return 0;
  }
