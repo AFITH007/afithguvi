9cf6f2e7f390541f6f491330d3d7d6c3d039e521
mohammedaafith <aafith007@gmail.com>
2019-02-12 12:05:53 +0530

    Update ch.java

--- ch.java
+++ ch.java
@@ -1,6 +1,6 @@
-import java.io.;
-import java.util.;
-public class Alphabet {
+import java.io.*;
+import java.util.*;
+class Alphabet {
   public static void main(String[] args) {
        Scanner sc=new Scanner(System.in);
 	     System.out.println("Enter a character : ");
