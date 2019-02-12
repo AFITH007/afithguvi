8f4a0c7ef69635ae1e4efbab2b4a7407d7bcc5c1
mohammedaafith <aafith007@gmail.com>
2019-02-12 11:41:03 +0530

    Create evenodd.c

---
+++ evenodd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int num;
+printf("\n ENTER THE NUMBER");
+scanf("%d",&num);
+if(num%2==0)
+ printf("\n It is even");
+ else
+ printf("\n It is odd");
+ return 0;
+ }
