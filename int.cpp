09779553d75527811e9e98c478bc46650101c66d
mohammedaafith <aafith007@gmail.com>
2019-02-13 11:47:49 +0530

    Create int.cpp

---
+++ int.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N,K,sum = 0;
+    cin >> N;
+    cin>>K;
+    for (int i = 0; i <=K5 ; ++i) {
+        sum += i;
+    }
+    cout << sum;
+    return 0;
+}
