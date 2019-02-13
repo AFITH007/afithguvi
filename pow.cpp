278590c2ce76f687eb3d68272331f45d268608f1
mohammedaafith <aafith007@gmail.com>
2019-02-13 12:24:51 +0530

    Create pow.cpp

---
+++ pow.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main ()
+{
+	double base, exponent, result;
+	
+	cin>>base;
+	cin>>exponent;
+	result = pow(base, exponent);
+	cout << result;
+return 0;
+}
+  
